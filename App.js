a751840c7af4e59f227d3e2d9efdcf429e4d2ed7
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-10-06 23:46:58 +0200

    second commit

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [product, setProduct] = useState("");
+  const [myProducts, setMyProducts] = useState([]);
+
+  const inputHandler = (val) => {
+    setProduct(val);
+  };
+
+  const submitHandler = () => {
+    setMyProducts((currentMyProducts) => [...currentMyProducts, product]);
+    setProduct("");
+  };
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Nouveau produit" />
-        <Button title="valider" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Nouveau produit"
+          onChangeText={inputHandler}
+          value={product}
+        />
+        <Button title="valider" onPress={submitHandler} />
+      </View>
+      <View style={styles.items}>
+        {myProducts.map((product, index) => {
+          return (
+            <Text style={styles.element} key={index}>
+              {product}
+            </Text>
+          );
+        })}
       </View>
     </View>
   );
@@ -27,5 +52,14 @@ const styles = StyleSheet.create({
     paddingLeft: 9,
     fontSize: 18,
     flexGrow: 1,
+  },
+  items: {
+    marginTop: 10,
+  },
+  element: {
+    backgroundColor: "#ffb6c1",
+    padding: 20,
+    fontSize: 17,
+    marginVertical: 6,
   },
 });
